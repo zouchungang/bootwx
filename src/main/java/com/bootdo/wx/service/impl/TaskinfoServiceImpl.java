1d61c62ade27f6dce1c17ce8d9f7b15840f4aec3
zoucg <804188876@qq.com>
2018-12-31 15:40:19 +0800

    微任务

--- src/main/java/com/bootdo/wx/service/impl/TaskinfoServiceImpl.java
+++ src/main/java/com/bootdo/wx/service/impl/TaskinfoServiceImpl.java
@@ -1,19 +1,25 @@
 package com.bootdo.wx.service.impl;
 
+import com.bootdo.common.aspect.LogAspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
 import com.bootdo.wx.dao.TaskinfoDao;
 import com.bootdo.wx.domain.TaskinfoDO;
 import com.bootdo.wx.service.TaskinfoService;
-
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
 
 @Service
 public class TaskinfoServiceImpl implements TaskinfoService {
+	private static final Logger logger = LoggerFactory.getLogger(LogAspect.class);
 	@Autowired
 	private TaskinfoDao taskinfoDao;
 	
@@ -33,8 +39,18 @@ public class TaskinfoServiceImpl implements TaskinfoService {
 	}
 	
 	@Override
+	@Transactional
 	public int save(TaskinfoDO taskinfo){
-		return taskinfoDao.save(taskinfo);
+		try {
+			BigDecimal totalmoney = taskinfo.getPrice().multiply(new BigDecimal(taskinfo.getNum()));
+			taskinfo.setTotalmoney(totalmoney);
+			return taskinfoDao.save(taskinfo);
+		}catch (Exception e){
+			e.printStackTrace();
+			logger.error("com.bootdo.wx.service.impl.TaskinfoServiceImpl.save->exception!message:{},cause:{},detail{}", e.getMessage(), e.getCause(), e.toString());
+			TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+			return -1;
+		}
 	}
 	
 	@Override
